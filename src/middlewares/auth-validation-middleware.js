68080ae131ee31174562029d6de4dd13c226f285
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2024-06-26 21:01:56 +0530

    feat:auth middleware implemented

---
+++ src/middlewares/auth-validation-middleware.js
@@ -0,0 +1,17 @@
+const AuthValidation=(req,res,next)=>{
+     
+      if( !req.body.email ||  !req.body.password ){
+        return res.status(501).json({
+            data:null,
+            success:false,
+            message:'Email or Password is missing',
+            error:"Email or pass is missing",
+        })
+      }
+
+      next();
+}
+
+module.exports={
+    AuthValidation 
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,18 @@
 const express=require('express');
 const router=express.Router();
 
+const {AuthRequestValidators}=require('../../middlewares/index');
+
 const UserController=require('../../controllers/user-controller');
 
-router.post('/user', UserController.create);
-router.post('/signin',UserController.signin);
+router.post('/user',
+    AuthRequestValidators.AuthValidation,
+     UserController.create
+    );
+router.post('/signin',
+    AuthRequestValidators.AuthValidation,
+    UserController.signin
+);
 
 
 module.exports=router
